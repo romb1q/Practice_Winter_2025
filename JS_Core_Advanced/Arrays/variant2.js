109e36ee476a6a868bead634e7f760d96ca24101
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-17 13:46:05 +0300

    JS Core Advanced

--- JS_Core_Advanced/Arrays/variant2.js
+++ JS_Core_Advanced/Arrays/variant2.js
@@ -1,1 +1,10 @@
+function sumOfArraysHalf(array) {
+    let sum = 0;
+    let firstHalf = array.length / 2;
+    for (let i = 0; i < firstHalf; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
 
+console.log(sumOfArraysHalf([89, 2, 3, 4, 5, 6, 7, 42, 9, 10, 52, 12, 13, 14, 15]));  // 158

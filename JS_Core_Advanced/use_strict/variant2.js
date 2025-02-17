3d381b29b52bb94990311946ab0b5a5fd25e1fca
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-17 13:39:21 +0300

    JS Core Advanced

--- JS_Core_Advanced/use_strict/variant2.js
+++ JS_Core_Advanced/use_strict/variant2.js
@@ -1,10 +1,5 @@
 'use strict';
 function findValueInArray(array, value) {
-    if (array.find(el => el === value ? true : false)) {
-        return true;
-    }
-    else{
-        return false;
-    }
+    return array.find(el => el === value) ? true : false
 }
 console.log(findValueInArray([1,2,3,4,5,6], 5));

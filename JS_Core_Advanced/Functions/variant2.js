174d4ef574506f2b0217bc80605d0bbbb4d181e8
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-17 13:51:48 +0300

    JS Core Advanced

--- JS_Core_Advanced/Functions/variant2.js
+++ JS_Core_Advanced/Functions/variant2.js
@@ -1,1 +1,13 @@
-
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+console.log(bubbleSort([1,4,2,7,8,-2]));  // [ -2, 1, 2, 4, 7, 8 ]

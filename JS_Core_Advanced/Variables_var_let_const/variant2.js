c2f8fb1a40328d29def2a3f522278729ab1e90fa
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-17 13:41:56 +0300

    JS Core Advanced

--- JS_Core_Advanced/Variables_var_let_const/variant2.js
+++ JS_Core_Advanced/Variables_var_let_const/variant2.js
@@ -1,1 +1,11 @@
+let x = 2;
+var y = 3;
+const z = 4;
 
+let x = 20;
+var y = 30;
+const z = 40;
+
+console.log(x);  // Identifier 'x' has already been declared
+console.log(y);  // 30
+console.log(z);  // Identifier 'z' has already been declared

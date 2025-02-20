3f1b0caf7f129064e575ad9b02595d15b395cd42
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-20 21:43:46 +0300

    JS Core Advanced

--- JS_Core_Advanced/Promise/variant2.js
+++ JS_Core_Advanced/Promise/variant2.js
@@ -1,1 +1,25 @@
+const urlsArray = [
+    "https://letterboxd.com/",
+    "https://www.hltv.org/",
+    "https://www.youtube.com/"
+];
 
+function fetchContent(url) {
+    return fetch(url).then(response => response.text());
+}
+
+function fetchAllContents(urlsArray) {
+    const promises = urlsArray.map(url => fetchContent(url));
+    return Promise.all(promises);
+}
+
+fetchAllContents(urlsArray)
+    .then(contents => {
+        contents.forEach((content, index) => {
+            console.log(`содержимое сайта ${index + 1}:`);
+            console.log(content);
+        });
+    })
+    .catch(error => {
+        console.error('ошибка содержимого:', error);
+    });

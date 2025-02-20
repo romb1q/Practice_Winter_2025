ef5a008ac7ed369c9f0f692462a1e83246175f07
Roman Biruk <105756991+romb1q@users.noreply.github.com>
2025-02-20 21:41:34 +0300

    JS Core Advanced

--- JS_Core_Advanced/Error_Handling/variant2.js
+++ JS_Core_Advanced/Error_Handling/variant2.js
@@ -1,1 +1,18 @@
+function checkProperty(obj, prop) {
+    try {
+        let value = obj[prop];
+        return value;
+    } catch (error) {
+        if (error instanceof TypeError) {
+            return `TypeError: '${prop}' не существует`;
+        } else {
+            throw error;
+        }
+    }
+}
 
+let obj1 = { name: "Gregor", age: 52 };
+let obj2;
+
+console.log(checkProperty(obj1, "name")); // Gregor
+console.log(checkProperty(obj2, "name")); // TypeError: 'name' не существует
